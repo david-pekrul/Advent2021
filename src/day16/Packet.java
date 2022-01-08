76b6b4073f0c332aeda2d73822dd3547d5a73161
David Pekrul <dbighead77@gmail.com>
2022-01-07 19:54:45 -0600

    Day 16 solved.

--- src/day16/Packet.java
+++ src/day16/Packet.java
@@ -1,32 +1,221 @@
 package day16;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Packet {
+public abstract class Packet {
     public int version;
-    public int operatorId;
+    //    public int operatorId;
     List<Packet> subpackets;
-    public int literal;
+    public long literal;
+//    public IntegerOps opFunc;
 
-    public Packet(int v, int o, int l){
+    static final Map<Integer, String> typeIdToOperator = new HashMap<>();
+
+    static {
+        typeIdToOperator.put(0, "+");
+        typeIdToOperator.put(1, "*");
+        typeIdToOperator.put(2, "min");
+        typeIdToOperator.put(3, "max");
+        typeIdToOperator.put(4, "int");
+        typeIdToOperator.put(5, ">");
+        typeIdToOperator.put(6, "<");
+        typeIdToOperator.put(7, "==");
+    }
+
+    public interface IntegerOps {
+        int apply(int a, int b);
+    }
+
+
+    public Packet(int v, long l) {
         this.version = v;
-        this.operatorId = o;
         this.literal = l;
         subpackets = List.of();
     }
 
-    public Packet(int v, int o, List<Packet> p){
+    public Packet(int v, List<Packet> p) {
         this.version = v;
-        this.operatorId = o;
         this.subpackets = p;
-        this.literal = Integer.MIN_VALUE;
+        this.literal = -1;
     }
 
-    public int getVersionSum(){
+    public abstract long calc();
+
+    public int getVersionSum() {
         int sum = version;
         for (Packet subpacket : subpackets) {
             sum += subpacket.getVersionSum();
         }
         return sum;
     }
+
+
+    public static class Factory {
+
+        public Packet build(int v, long literal) {
+            return new LiteralPacket(v, literal);
+        }
+
+        public Packet build(int v, int o, List<Packet> subPackets) {
+            switch (o) {
+                case 0:
+                    return new SumPacket(v, subPackets);
+                case 1:
+                    return new MultiplyPacket(v, subPackets);
+                case 2:
+                    return new MinPacket(v, subPackets);
+                case 3:
+                    return new MaxPacket(v, subPackets);
+                case 5:
+                    return new GreaterThanPacket(v, subPackets);
+                case 6:
+                    return new LessThanPacket(v, subPackets);
+                case 7:
+                    return new EqualsPacket(v, subPackets);
+            }
+            throw new RuntimeException("Unrecognized Operation Type: " + o);
+        }
+
+
+    }
+
+    private static class SumPacket extends Packet {
+        public SumPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            if (this.subpackets.size() == 1) {
+                return this.subpackets.get(0).calc();
+            }
+            List<Long> calcStream = this.subpackets.stream().map(Packet::calc).collect(Collectors.toList());
+            long answer = calcStream.stream().reduce(0l, Long::sum);
+            if (answer < 0) {
+                throw new RuntimeException("answer is negative");
+            }
+            return answer;
+        }
+    }
+
+    private static class MultiplyPacket extends Packet {
+        public MultiplyPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            List<Long> calcStream = this.subpackets.stream().map(Packet::calc).collect(Collectors.toList());
+            long answer = calcStream.stream().reduce(1l, (a, b) -> a * b);
+            if (answer < 0) {
+                throw new RuntimeException("answer is negative");
+            }
+            return answer;
+        }
+    }
+
+    private static class MinPacket extends Packet {
+        public MinPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            if (this.subpackets.size() == 1) {
+                return this.subpackets.get(0).calc();
+            }
+            List<Long> calcStream = this.subpackets.stream().map(Packet::calc).collect(Collectors.toList());
+            long answer = calcStream.stream().reduce(Long.MAX_VALUE, (a, b) -> Long.min(a, b));
+            if (answer < 0) {
+                throw new RuntimeException("answer is negative");
+            }
+            return answer;
+        }
+    }
+
+    private static class MaxPacket extends Packet {
+        public MaxPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            if (this.subpackets.size() == 1) {
+                return this.subpackets.get(0).calc();
+            }
+            List<Long> calcStream = this.subpackets.stream().map(Packet::calc).collect(Collectors.toList());
+            long answer = calcStream.stream().reduce(0l, (a, b) -> Long.max(a, b));
+            if (answer < 0) {
+                throw new RuntimeException("answer is negative");
+            }
+            return answer;
+        }
+    }
+
+    private static class GreaterThanPacket extends Packet {
+        public GreaterThanPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            if (subpackets.size() > 2) {
+                throw new RuntimeException("too many subpackets");
+            }
+            long left = subpackets.get(0).calc();
+            long right = subpackets.get(1).calc();
+            return left > right ? 1 : 0;
+        }
+    }
+
+    private static class LessThanPacket extends Packet {
+        public LessThanPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            if (subpackets.size() > 2) {
+                throw new RuntimeException("too many subpackets");
+            }
+            long left = subpackets.get(0).calc();
+            long right = subpackets.get(1).calc();
+            return left < right ? 1 : 0;
+        }
+    }
+
+    private static class EqualsPacket extends Packet {
+        public EqualsPacket(int v, List<Packet> p) {
+            super(v, p);
+        }
+
+        @Override
+        public long calc() {
+            if (subpackets.size() > 2) {
+                throw new RuntimeException("too many subpackets");
+            }
+            long left = subpackets.get(0).calc();
+            long right = subpackets.get(1).calc();
+            return left == right ? 1 : 0;
+        }
+    }
+
+    private static class LiteralPacket extends Packet {
+        public LiteralPacket(int version, long literal) {
+
+            super(version, literal);
+            if (literal < 0) {
+                throw new RuntimeException("literal is negative");
+            }
+        }
+
+        @Override
+        public long calc() {
+            return this.literal;
+        }
+    }
+
 }
--- src/day16/PacketParser.java
+++ src/day16/PacketParser.java
@@ -1,14 +1,14 @@
 package day16;
 
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class PacketParser {
 
     static final Map<String, String> hexToBinary = new HashMap<>();
-    static final Map<Integer, String> typeIdToOperator = new HashMap<>();
-    static final Map<Integer, IntegerOps> thing = new HashMap<>();
+    static final Packet.Factory packetFactory = new Packet.Factory();
 
 
     static {
@@ -29,35 +29,9 @@ public class PacketParser {
         hexToBinary.put("E", "1110");
         hexToBinary.put("F", "1111");
 
-        typeIdToOperator.put(0, "+");
-        typeIdToOperator.put(1, "*");
-        typeIdToOperator.put(2, "min");
-        typeIdToOperator.put(3, "max");
-        typeIdToOperator.put(5, ">");
-        typeIdToOperator.put(6, "<");
-        typeIdToOperator.put(7, "==");
-
-        thing.put(0, (a, b) -> a + b);
-        thing.put(1, (a, b) -> a * b);
-        thing.put(2, (a, b) -> Math.min(a, b));
-        thing.put(3, (a, b) -> Math.max(a, b));
-        thing.put(5, (a, b) -> a > b ? 1 : 0);
-        thing.put(6, (a, b) -> a < b ? 1 : 0);
-        thing.put(7, (a, b) -> a == b ? 1 : 0);
-
 
     }
 
-    public interface IntegerOps {
-        int apply(int a, int b);
-    }
-
-
-    public PacketParser() {
-        int a = 3;
-        int b = 5;
-        thing.get(0).apply(a,b);
-    }
 
     public static Packet parse(String rawInput) {
         StringBuilder sb = new StringBuilder();
@@ -67,7 +41,7 @@ public class PacketParser {
 
         String rawBinary = sb.toString();
 
-        LinkedList<Character> bits = new LinkedList<Character>();
+        LinkedList<Character> bits = new LinkedList<>();
         for (char c : rawBinary.toCharArray()) {
             bits.add(c);
         }
@@ -78,18 +52,18 @@ public class PacketParser {
     private static Packet parse(LinkedList<Character> remainingBits) {
         //packet always starts with 3 bits as the version
         //packet then has 3 bits as the operator
-        int version = binaryToDecimal(takeNBits(remainingBits, 3));
-        int typeId = binaryToDecimal(takeNBits(remainingBits, 3));
+        int version = (int)binaryToDecimal(takeNBits(remainingBits, 3));
+        int typeId = (int) binaryToDecimal(takeNBits(remainingBits, 3));
 
         if (typeId == 4) { //this is a literal value
-            return new Packet(version, typeId, parseLiteral(remainingBits));
+            return packetFactory.build(version,parseLiteral(remainingBits));
         }
 
         char i = remainingBits.pop();
         List<Packet> subPackets = new LinkedList<>();
 
         if (i == '0') {
-            int numberOfBitsInSubpackets = binaryToDecimal(takeNBits(remainingBits, 15));
+            int numberOfBitsInSubpackets = (int) binaryToDecimal(takeNBits(remainingBits, 15));
             LinkedList<Character> subList = createSubList(remainingBits, numberOfBitsInSubpackets);
             while (!subList.isEmpty()) {
                 subPackets.add(parse(subList));
@@ -100,18 +74,18 @@ public class PacketParser {
         } else {
             //i == 1
             //the next 11 bits are the binary for how many subpackets there are
-            int numberOfSubpackets = binaryToDecimal(takeNBits(remainingBits, 11));
+            long numberOfSubpackets = binaryToDecimal(takeNBits(remainingBits, 11));
             for (int p = 0; p < numberOfSubpackets; p++) {
                 Packet subPacket = parse(remainingBits);
                 subPackets.add(subPacket);
             }
         }
 
-        return new Packet(version, typeId, subPackets);
+        return packetFactory.build(version,typeId,subPackets);
     }
 
-    public static int binaryToDecimal(String binary) {
-        int num = 0;
+    public static long binaryToDecimal(String binary) {
+        long num = 0;
         for (int i = binary.length() - 1; i >= 0; i--) {
             if (binary.charAt(i) == '1') {
                 num += Math.pow(2, binary.length() - 1 - i);
@@ -137,7 +111,7 @@ public class PacketParser {
         return subList;
     }
 
-    public static int parseLiteral(LinkedList<Character> bits) {
+    public static long parseLiteral(LinkedList<Character> bits) {
         StringBuilder sb = new StringBuilder();
         while (bits.pop().equals('1')) {
             for (int i = 0; i < 4; i++) {
